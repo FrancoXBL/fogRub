bc9e0d778bc97a9ea76bccfa9aaf01b073c78d50
Franco Xavier Bonin Lara <131810349+FrancoXBL@users.noreply.github.com>
2025-08-20 21:49:10 -0300

    Add files via upload

--- config/dayDate.js
+++ config/dayDate.js
@@ -3,3 +3,8 @@ export const fechaActual = new Date().toLocaleDateString('es-ES', {
     month: '2-digit',
     year: 'numeric'
   }).split('/').join('-'); // Formato 'DD-MM-YYYY'
+
+export const mesActual = new Date().toLocaleDateString('es-ES', {
+    month: '2-digit',
+    year: 'numeric'
+  }).split('/').join('-'); // Formato 'MM-YYYY'
